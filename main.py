360c3e1c51f0c89bc56a466da2653c27f69cef1f
Narges <narges.moharrami97@gmail.com>
2021-11-14 11:24:47 -0800

    HouseRent class and ApartmentRent class added. HouseSale class and ApartmentSale class added.

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 class Apartment:
-    def __init__(self, floor, elevator):
+    def __init__(self, floor, elevator, *args, **kwargs):
         self.floor = floor
         self.elevator = elevator
+        super().__init__(*args, **kwargs)
+
 
 class House:
-    def __init__(self, age, address):
+    def __init__(self, age, address, *args, **kwargs):
         self.age = age
         self.address = address
+        super().__init__(*args, **kwargs)
+
 
 class Rent:
     def __init__(self, fixed_amount, monthly_amount):
         self.fixed_amount = fixed_amount
         self.monthly_amount = monthly_amount
 
-class sale:
+
+class Sale:
     def __init__(self, fee):
         self.fee = fee
+
+
+class ApartmentRent(Apartment, Rent):
+    def __str__(self):
+        return f"floor: {self.floor} \t fixed_amount: {self.fixed_amount}"
+
+
+class ApartmentSale(Apartment, Sale):
+    def __str__(self):
+        return f"floor: {self.floor} \t fee: {self.fee}"
+
+
+class HouseRent(House, Rent):
+    pass
+
+
+class HouseSale(House, Sale):
+    pass
