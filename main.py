5c965000c1c450f605f972a9196ac95bed113d7f
Narges <narges.moharrami97@gmail.com>
2021-11-14 11:00:45 -0800

    class House and Apartment added

---
+++ main.py
@@ -0,0 +1,18 @@
+class Apartment:
+    def __init__(self, floor, elevator):
+        self.floor = floor
+        self.elevator = elevator
+
+class House:
+    def __init__(self, age, address):
+        self.age = age
+        self.address = address
+
+class Rent:
+    def __init__(self, fixed_amount, monthly_amount):
+        self.fixed_amount = fixed_amount
+        self.monthly_amount = monthly_amount
+
+class sale:
+    def __init__(self, fee):
+        self.fee = fee
