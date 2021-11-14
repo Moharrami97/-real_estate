99227617259060c8686db830f58302cfa36b3d61
Narges <narges.moharrami97@gmail.com>
2021-11-14 11:59:58 -0800

    using abstraction and create show_banner

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from abc import ABC, abstractmethod
+
+
 class Apartment:
     def __init__(self, floor, elevator, *args, **kwargs):
         self.floor = floor
@@ -12,30 +15,52 @@ class House:
         super().__init__(*args, **kwargs)
 
 
-class Rent:
+class Rent(ABC):
     def __init__(self, fixed_amount, monthly_amount):
         self.fixed_amount = fixed_amount
         self.monthly_amount = monthly_amount
 
+    @abstractmethod
+    def show_banner(self):
+        pass
 
-class Sale:
+
+class Sale(ABC):
     def __init__(self, fee):
         self.fee = fee
+
+    @abstractmethod
+    def show_banner(self):
+        pass
 
 
 class ApartmentRent(Apartment, Rent):
     def __str__(self):
         return f"floor: {self.floor} \t fixed_amount: {self.fixed_amount}"
 
+    def show_banner(self):
+        return "show banner in ApartmentRent"
+
 
 class ApartmentSale(Apartment, Sale):
     def __str__(self):
         return f"floor: {self.floor} \t fee: {self.fee}"
 
+    def show_banner(self):
+        return "show banner in ApartmentSale"
+
 
 class HouseRent(House, Rent):
-    pass
+    def __str__(self):
+        return f"address: {self.address} \t fixed_amount: {self.fixed_amount}"
+
+    def show_banner(self):
+        return "show banner in HouseRent"
 
 
 class HouseSale(House, Sale):
-    pass
+    def __str__(self):
+        return f"address: {self.address} \t fixed_amount: {self.fee}"
+
+    def show_banner(self):
+        return "show banner in HouseSale"
--- real_estate.py
+++ real_estate.py
@@ -3,6 +3,10 @@ from main import *
 if __name__ == "__main__":
     apartment_rent = ApartmentRent(floor=2, elevator=True, fixed_amount=50000000, monthly_amount=1000000)
     apartment_sale = ApartmentSale(floor=2, elevator=True, fee=500000000)
+    house_rent = HouseRent(age=8, address="tehran", fixed_amount=60000000, monthly_amount=2000000)
+    house_sale = HouseSale(age=8, address="tehran", fee=600000000)
 
     print(apartment_rent)
     print(apartment_sale)
+    print(house_rent)
+    print(house_sale)
