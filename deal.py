e19bc6e063a89711a19df71344339f98d686898b
Narges <narges.moharrami97@gmail.com>
2021-11-16 11:48:53 -0800

    specify the seller or rent

--- deal.py
+++ deal.py
@@ -1,17 +1,18 @@
-class Rent:
-    def __init__(self, fixed_amount, monthly_amount):
-        self.fixed_amount = fixed_amount
-        self.monthly_amount = monthly_amount
-
-    @abstractmethod
-    def show_banner(self):
-        pass
+from base import BaseClass
 
 
-class Sale(ABC):
-    def __init__(self, fee):
-        self.fee = fee
+class Rent(BaseClass):
+    def __init__(self, initial_price, monthly_price, convertable, discountable, *args, **kwargs):
+        self.initial_price = initial_price
+        self.monthly_price = monthly_price
+        self.convertable = convertable
+        self.discountable = discountable
+        super().__init__(*args, **kwargs)
 
-    @abstractmethod
-    def show_banner(self):
-        pass
+
+class Sale(BaseClass):
+    def __init__(self, price_per_meter, convertable, discountable, *args, **kwargs):
+        self.price_per_meter = price_per_meter
+        self.convertable = convertable
+        self.discountable = discountable
+        super().__init__(*args, **kwargs)
