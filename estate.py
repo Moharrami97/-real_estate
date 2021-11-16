0d9becb3552e8f429e399e759ff72e1b3b8a7b3f
Narges <narges.moharrami97@gmail.com>
2021-11-16 11:19:18 -0800

    estate class abstracted

--- estate.py
+++ estate.py
@@ -1,4 +1,5 @@
 from base import BaseClass
+from abc import abstractmethod
 
 
 class EstateAbstract(BaseClass):
@@ -11,14 +12,32 @@ class EstateAbstract(BaseClass):
         self.address = address
         super().__init__(*args, **kwargs)
 
+    @abstractmethod
+    def show_description(self):
+        pass
+
 
 class Apartment(EstateAbstract):
-    pass
+    def __init__(self, has_elevator, has_parking, floor, *args, **kwargs):
+        self.has_elevator = has_elevator
+        self.has_parking = has_parking
+        self.floor = floor
+        super().__init__(*args, **kwargs)
+
+    def show_description(self):
+        print(f"Apartment {self.id}")
 
 
 class House(EstateAbstract):
-    pass
+    def __init__(self, has_yard, floors_count, *args, **kwargs):
+        self.has_yard = has_yard
+        self.floors_count = floors_count
+        super().__init__(*args, **kwargs)
+
+    def show_description(self):
+        print(f"House {self.id}
 
 
 class Store(EstateAbstract):
-    pass
+    def show_description(self):
+        print(f"store {self.id}
